7727b183c2e15b688ab3f5727171f62d8e760fd4
Laurence Wayne <laurence.wayne@outlook.com>
2026-03-23 16:25:40 +0000

    adding analytics tracking

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
 import MapView from "./components/MapView";
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/map" element={<MapView />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/map" element={<MapView />} />
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
   );
 }
